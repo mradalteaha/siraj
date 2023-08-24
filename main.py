48684e16343065c153fb291990cf53567f2525bb
Morad Teaha <mrad1999@gmail.com>
2023-08-24 22:05:20 +0300

    finished A section

--- main.py
+++ main.py
@@ -14,92 +14,75 @@ Queue_Size = 3
 tasks_path = './tasks'
 all_tasks = dict()  # completed and uncompleted
 completed_task = dict()
+main_lock = threading.Lock()
 
-
-class TaskQueue:
-    def __init__(self):
-        self.task_queue = queue.Queue()
-        self.is_processing = False
-        self.max_size = Queue_Size
-        self.current_tasks = 0
-        self.waiting_queue = queue.Queue()
-
-    def enqueue_task(self, task_function):
-        with threading.Lock():
-
-            if self.current_tasks < self.max_size:
-                self.task_queue.put(task_function)
-                self.current_tasks = self.current_tasks + 1
-                print("printing task on queue")
-                print(list(self.task_queue.queue))
-            elif self.current_tasks >= self.max_size:
-                self.waiting_queue.put(task_function)
-                print("printing task on waiting")
-                print(list(self.waiting_queue.queue))
-
-    def enqueue_waiting_task(self, task_function):
-        with threading.Lock():
-            self.waiting_queue.put(task_function)
-
-
-processing_queue = TaskQueue()  # creating global instance of the queue
+processing_queue = queue.Queue(Queue_Size)  # creating global instance of the queue
 
 
 #this function is responsible for executing the tasks in the queue
-def process_next_task(myQueue):
-    print('processing tasks')
-    if not myQueue.is_processing and not myQueue.task_queue.empty():
-        myQueue.is_processing = True
-        task_function = myQueue.task_queue.get()  # poping the top of the queue
-        if task_function:
-            task_to_process(task_function)
-
-            # the task in the top of the queue is done
-        myQueue.is_processing = False
-        myQueue.current_tasks = myQueue.current_tasks - 1
-        if not myQueue.waiting_queue.empty():
-            myQueue.enqueue_task(myQueue.waiting_queue.get())
-        process_next_task(myQueue)
+def process_next_task(myQueue,tasks_list):#consumer
+    while True:
+        if not myQueue.empty():
+            #print('performing a task')
+            #print(list(myQueue.task_queue.queue))
+            #critical section
+            main_lock.acquire()
+            task_function = myQueue.get()  # poping the top of the queue
+            tasks_list[task_function][0]=1
+            main_lock.release()
+            if task_function:
+                task_to_process(task_function)
+                myQueue.task_done()
+            
 
 
 #this function is responsible for importing the new tasks or already one from tasks folder
-def get_all_tasks(myQueue):
+def get_all_tasks(myQueue,tasks_list): #producer
     while True:
         for file_path in os.listdir(tasks_path):
             # check if current file_path is a file
             if os.path.isfile(os.path.join(tasks_path, file_path)):
                 # add filename to list
-                if not all_tasks.get(file_path):  # it's new uncompleted task
+                if not tasks_list.get(file_path):  # it's new uncompleted task
+                    #print("adding the task to queue for the first time" , file_path)
+                    main_lock.acquire()
+                    tasks_list[file_path] = [0, file_path]
+                    main_lock.release()
+                    myQueue.put(file_path)
+                    print("tasks in queue")
+                    print(list(myQueue.queue))
+                    
 
-                    all_tasks[file_path] = [0, file_path]
-                if all_tasks.get(file_path) and all_tasks.get(file_path)[0] == 0:
-                    all_tasks[file_path][0] = 1
+       
 
-                    myQueue.enqueue_task(file_path)  # the task either being handled or in waiting queue
-                    #print(list(processing_queue.task_queue.queue))
 
-        sleep(5)
+
 
 
 def task_to_process(taskname):
-    with open("./tasks/" + taskname) as f:
-        exec(f.read())
-
+    try:
+        
+        with open("./tasks/" + taskname) as f:
+            exec(f.read())
+    except:
+        print("error on task to process")
 
 
 def main():
     try:
         # two threads one for importing the tasks from the file and the other to execute
-        importing_thread = Thread(target=get_all_tasks, args=(processing_queue,))
+        importing_thread = Thread(target=get_all_tasks, args=(processing_queue,all_tasks,))
         importing_thread.start()
 
-        processing_thread = Thread(target=process_next_task, args=(processing_queue,))
+        processing_thread = Thread(target=process_next_task, args=(processing_queue,all_tasks,))
 
         processing_thread.start()
         #killing the threads after two minutes
         importing_thread.join(120)
         processing_thread.join(120)
+        
         print('done')
+        print(all_tasks)
 
         return 0
     except:
